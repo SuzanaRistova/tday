4361d625c249c2e9abb07cc81ae74f6b59ee722f
Suzana <Suzana@Suzana-PC>
2014-01-20 01:16:08 +0100

    posedno

--- add_post.php
+++ add_post.php
@@ -1,6 +1,5 @@
 <?php
 session_start();
-$id = $_SESSION['id'];
 $mysql_hostname = "localhost";
 $mysql_user = "root";
 $mysql_password = "";
@@ -13,15 +12,26 @@ $bd = mysql_connect($mysql_hostname, $mysql_user, $mysql_password) or die("Could
 mysql_select_db($mysql_database, $bd) or die("Could not select database");
 
 
-// get data that sent from form 
-
-
-$naslov=$_POST['naslov'];
-$sodrzina=$_POST['sodrzina'];
-$sql="INSERT INTO post(id,naslov, sodrzina,users_id)VALUES(NULL,'$naslov', '$sodrzina',$id)";
-$result=mysql_query($sql);
-
-if($result){
+ 
+//if(isset($_POST['id']))
+    {
+        $users_id = $_POST['id'];
+      
+        $query1 = "SELECT * FROM users";
+       $result1=mysql_query($query1);
+        $red1 = mysql_fetch_array($result1);
+		$naslov=$_POST['naslov'];
+		$sodrzina=$_POST['sodrzina'];
+		if($result1){
+		echo "Successful<BR>";
+echo "<a href=main_forum.php>View your topic</a>";
+}
+else {
+echo "ERROR";
+}
+        $query2 = "INSERT INTO post (id, naslov,sodrzina, users_id) VALUES (NULL, '$naslov', '$sodrzina', '$id')";
+        $result2 = mysql_query($query2);
+		if($result2){
 echo "Successful<BR>";
 echo "<a href=main_forum.php>View your topic</a>";
 }
@@ -29,4 +39,13 @@ else {
 echo "ERROR";
 }
 mysql_close();
+    
+     
+    
+    
+}
+
 ?>
+
+
+ 
--- checklogin.php
+++ checklogin.php
@@ -20,13 +20,20 @@ $encrypted_mypassword=md5($mypassword);
 $sql="SELECT * FROM users WHERE uname='$myusername' and pass='$encrypted_mypassword'";
 $result=mysql_query($sql);
 
+
 		
 // Mysql_num_row is counting table row
 $count=mysql_num_rows($result);
 
-//Nesigurno sledni 2 reda
-$red1=mysql_fetch_row($result1);
-$id = $red1['id'];
+
+/*$query1 = "SELECT * FROM users";
+$result1=mysql_query($query1);
+$red1=mysql_fetch_row($result1)
+ $id = $red1['id'];
+ var_dump($id);
+ }
+ */
+
 
 // If result matched $myusername and $mypassword, table row must be 1 row
 if($count==1){
